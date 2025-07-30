2bcb00a222eacf8182beb62670efda426706fe17
Beatriz Jacinto de Almeida <106273381+profBia@users.noreply.github.com>
2025-07-30 18:36:50 -0300

    0

---
+++ script.js
@@ -0,0 +1,20 @@
+function inserirNome () {
+    let nomeUsuario = prompt("Qual o seu nome");
+    let elemento = document.querySelector("#nome-usuário");
+    elemento.textContent = nomeUsuario;
+}
+inserirNome();
+
+let linguagens = ["JavaScript", "Python", "C++"];
+console.log(linguagens[1])
+
+const item = document.querySelector("#lista");
+//item.textContent = linguagens[1];
+item.textContent = `${linguagens[0]}, ${linguagens[1]} e ${linguagens[2]}`;
+
+let aluno { 
+    nome: Gabriel
+    idade: 13,
+    anoLetivo: "2ªsérie",
+    materiasFavoritas: ["Geografia","Programação","Arte"]
+}
